5ec83ef7edd169f9bf271b0e56cd76433efc2a22
seul320 <dlwlals3456@naver.com>
2023-10-17 20:41:39 +0900

    231017 -1

---
+++ 6-8-1.js
@@ -0,0 +1,10 @@
+let s = 1 + 2 + "hello" + 3;
+let t = 1 + "hello" + 2 + 3;
+
+console.log(s);
+console.log(t);
+
+//cd webtest > node test.js (node C:\Users\dlwla\Desktop\git\webTEST\test.js)
+
+//3hello3
+//1hello23
---
+++ 6-8-2.js
@@ -0,0 +1,7 @@
+let s = ("hello" > "javascript");
+let t = ("아버지" > " 어머니");
+
+console.log(s);
+console.log(t);
+
+//유니코드로 따졌을 때 더 크고 작은걸로 구분해서 결과가 정해짐
